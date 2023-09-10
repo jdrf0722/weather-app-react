ad78ec8d3c60e007a411f779624191c0846b2eba
Jose Daniel Rojas <jdrf0722@gmail.com>
2023-09-09 21:26:35 -0600

    add timeandlocation

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
 import Inputs from "./components/Inputs";
+import TimeAndLocation from "./components/TimeAndLocation";
 import TopButtons from "./components/TopButtons";
 
 function App() {
   return (
     <>
       <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from bg-cyan-700 to to-blue-700 h-fit shadow-xl shadow-gray-400">
-        <TopButtons/>
+        <TopButtons />
         <Inputs />
+        
+        <TimeAndLocation />
       </div>
     </>
   );
