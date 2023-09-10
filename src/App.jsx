b9c281eb3d6a1c2f2dfbb62ecca74c4eaf417e18
Jose Daniel Rojas <jdrf0722@gmail.com>
2023-09-09 21:11:05 -0600

    initial header

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
-function App() {
+import Inputs from "./components/Inputs";
+import TopButtons from "./components/TopButtons";
 
-  return <><h1 className="text-2xl font-bold">HOLA</h1></>;
+function App() {
+  return (
+    <>
+      <div className="mx-auto max-w-screen-md mt-4 py-5 px-32 bg-gradient-to-br from bg-cyan-700 to to-blue-700 h-fit shadow-xl shadow-gray-400">
+        <TopButtons/>
+        <Inputs />
+      </div>
+    </>
+  );
 }
 
 export default App;
